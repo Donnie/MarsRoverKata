a9b27af96b618a17d853c8de526dc7633a276568
Donnie <donnieashok@gmail.com>
2021-01-18 22:17:05 +0100

    Run Mission Success

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 ## Introduction
 Upon touching down the Mars Rover should relay it's correct location and direction.
 
-### Moving Forward
-When the rover moves forward in the extant direction, the coordinates change to reflect the correct movement.
+### Moving Forward and backward
+When the rover moves forward in the extant direction or it moves backward against the extant direction, the coordinates change to reflect the correct movement.
+
+### Rotating Left and Right
+When the rover rotates on its axis the direction the rover faces changes.
 
 ## Testing
 To run the tests do `make test`
--- rover/rover.go
+++ rover/rover.go
@@ -28,16 +28,16 @@ func NewRover(x, y int8, dir string) (r *Rover) {
 	}
 
 	r.drivers = make(map[string]Driver)
-	r.drivers["EAST"] = r.GoEast
-	r.drivers["WEST"] = r.GoWest
-	r.drivers["NORTH"] = r.GoNorth
-	r.drivers["SOUTH"] = r.GoSouth
+	r.drivers["EAST"] = r.goEast
+	r.drivers["WEST"] = r.goWest
+	r.drivers["NORTH"] = r.goNorth
+	r.drivers["SOUTH"] = r.goSouth
 
 	r.rotators = make(map[string]Rotator)
-	r.rotators["EAST"] = r.FromEast
-	r.rotators["WEST"] = r.FromWest
-	r.rotators["NORTH"] = r.FromNorth
-	r.rotators["SOUTH"] = r.FromSouth
+	r.rotators["EAST"] = r.fromEast
+	r.rotators["WEST"] = r.fromWest
+	r.rotators["NORTH"] = r.fromNorth
+	r.rotators["SOUTH"] = r.fromSouth
 
 	r.steppers = make(map[string]int8)
 	r.steppers["F"] = 1
@@ -46,95 +46,108 @@ func NewRover(x, y int8, dir string) (r *Rover) {
 	return
 }
 
-// Drive is the driving force
-func (r *Rover) Drive(com string) {
+// drive is the driving force
+func (r *Rover) drive(com string) {
 	r.drivers[r.Dir](r.steppers[com])
 }
 
-// GoEast takes the rover east
-func (r *Rover) GoEast(step int8) {
+// goEast takes the rover east
+func (r *Rover) goEast(step int8) {
 	r.LocY += step
 }
 
-// GoWest takes the rover west
-func (r *Rover) GoWest(step int8) {
+// goWest takes the rover west
+func (r *Rover) goWest(step int8) {
 	r.LocY -= step
 }
 
-// GoNorth takes the rover north
-func (r *Rover) GoNorth(step int8) {
+// goNorth takes the rover north
+func (r *Rover) goNorth(step int8) {
 	r.LocX += step
 }
 
-// GoSouth takes the rover south
-func (r *Rover) GoSouth(step int8) {
+// goSouth takes the rover south
+func (r *Rover) goSouth(step int8) {
 	r.LocX -= step
 }
 
-// Rotate is the rotating force
-func (r *Rover) Rotate(com string) {
+// rotate is the rotating force
+func (r *Rover) rotate(com string) {
 	r.rotators[r.Dir](com)
 }
 
-// FromEast rotates the rover east
-func (r *Rover) FromEast(com string) {
+// fromEast rotates the rover from east
+func (r *Rover) fromEast(com string) {
 	options := map[string]string{"L": "NORTH", "R": "SOUTH"}
 	r.Dir = options[com]
 }
 
-// FromWest rotates the rover west
-func (r *Rover) FromWest(com string) {
+// fromWest rotates the rover from west
+func (r *Rover) fromWest(com string) {
 	options := map[string]string{"L": "SOUTH", "R": "NORTH"}
 	r.Dir = options[com]
 }
 
-// FromNorth rotates the rover north
-func (r *Rover) FromNorth(com string) {
+// fromNorth rotates the rover from north
+func (r *Rover) fromNorth(com string) {
 	options := map[string]string{"L": "WEST", "R": "EAST"}
 	r.Dir = options[com]
 }
 
-// FromSouth rotates the rover south
-func (r *Rover) FromSouth(com string) {
+// fromSouth rotates the rover from south
+func (r *Rover) fromSouth(com string) {
 	options := map[string]string{"L": "EAST", "R": "WEST"}
 	r.Dir = options[com]
 }
 
-// Report outputs the current status of rover
-func (r *Rover) Report() string {
+// report outputs the current status of rover
+func (r *Rover) report() string {
 	return fmt.Sprintf(`(%d, %d) %s`, r.LocX, r.LocY, r.Dir)
 }
 
 // TouchDown on a Planet
 func (r *Rover) TouchDown() string {
-	return r.Report()
+	return r.report()
 }
 
 // Forward moves the rover one step in extant direction
 func (r *Rover) Forward() string {
-	r.Drive("F")
-	return r.Report()
+	r.drive("F")
+	return r.report()
 }
 
 // Backward moves the rover one step against extant direction
 func (r *Rover) Backward() string {
-	r.Drive("B")
-	return r.Report()
+	r.drive("B")
+	return r.report()
 }
 
 // RotateLeft rotates the rover 90° left
 func (r *Rover) RotateLeft() string {
-	r.Rotate("L")
-	return r.Report()
+	r.rotate("L")
+	return r.report()
 }
 
 // RotateRight rotates the rover 90° right
 func (r *Rover) RotateRight() string {
-	r.Rotate("R")
-	return r.Report()
+	r.rotate("R")
+	return r.report()
 }
 
-// RunMission runs multiple commands
+// RunMission runs multiple commands and provides report
 func (r *Rover) RunMission(com string) (report string) {
+	var comms = map[rune]func() string{
+		'F': r.Forward,
+		'B': r.Backward,
+		'R': r.RotateRight,
+		'L': r.RotateLeft,
+	}
+
+	for _, c := range com {
+		if val, ok := comms[c]; ok {
+			report = val()
+		}
+	}
+
 	return
 }
