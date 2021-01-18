55a72e1b6252da51f5d2bf4b22363bfcdbc142fd
Donnie <donnieashok@gmail.com>
2021-01-18 19:26:27 +0100

    Move Forward Success

--- rover/rover.go
+++ rover/rover.go
@@ -2,19 +2,60 @@ package rover
 
 import "fmt"
 
+// Driver takes rover in a direction
+type Driver func()
+
 // Rover properties
 type Rover struct {
 	LocX int8
 	LocY int8
 	Dir  string
+
+	drivers map[string]Driver
+}
+
+// Drive is the driving force
+func (r *Rover) Drive() {
+	r.drivers = make(map[string]Driver)
+	r.drivers["EAST"] = r.GoEast
+	r.drivers["WEST"] = r.GoWest
+	r.drivers["NORTH"] = r.GoNorth
+	r.drivers["SOUTH"] = r.GoSouth
+	r.drivers[r.Dir]()
+}
+
+// GoEast takes the rover east
+func (r *Rover) GoEast() {
+	r.LocY++
+}
+
+// GoWest takes the rover west
+func (r *Rover) GoWest() {
+	r.LocY--
+}
+
+// GoNorth takes the rover north
+func (r *Rover) GoNorth() {
+	r.LocX++
+}
+
+// GoSouth takes the rover south
+func (r *Rover) GoSouth() {
+	r.LocX--
+}
+
+// Report outputs the current status of rover
+func (r *Rover) Report() string {
+	return fmt.Sprintf(`(%d, %d) %s`, r.LocX, r.LocY, r.Dir)
 }
 
 // TouchDown on a Planet
 func (r *Rover) TouchDown() string {
-	return fmt.Sprintf(`(%d, %d) %s`, r.LocX, r.LocY, r.Dir)
+	return r.Report()
 }
 
 // Forward moves the rover one step in extant direction
-func (r *Rover) Forward() (out string) {
-	return
+func (r *Rover) Forward() string {
+	r.Drive()
+	return r.Report()
 }
