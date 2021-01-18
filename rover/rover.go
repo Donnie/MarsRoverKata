5019f8a2d7bfeb61ead2bc1adf5e4e5149af6e23
Donnie <donnieashok@gmail.com>
2021-01-18 20:00:27 +0100

    Move Backward Success

--- rover/rover.go
+++ rover/rover.go
@@ -3,7 +3,7 @@ package rover
 import "fmt"
 
 // Driver takes rover in a direction
-type Driver func()
+type Driver func(step int8)
 
 // Rover properties
 type Rover struct {
@@ -11,7 +11,8 @@ type Rover struct {
 	LocY int8
 	Dir  string
 
-	drivers map[string]Driver
+	drivers  map[string]Driver
+	steppers map[string]int8
 }
 
 // NewRover creates a new rover
@@ -24,33 +25,37 @@ func NewRover(x, y int8, dir string) Rover {
 }
 
 // Drive is the driving force
-func (r *Rover) Drive() {
+func (r *Rover) Drive(com string) {
+	r.steppers = make(map[string]int8)
+	r.steppers["F"] = 1
+	r.steppers["B"] = -1
+
 	r.drivers = make(map[string]Driver)
 	r.drivers["EAST"] = r.GoEast
 	r.drivers["WEST"] = r.GoWest
 	r.drivers["NORTH"] = r.GoNorth
 	r.drivers["SOUTH"] = r.GoSouth
-	r.drivers[r.Dir]()
+	r.drivers[r.Dir](r.steppers[com])
 }
 
 // GoEast takes the rover east
-func (r *Rover) GoEast() {
-	r.LocY++
+func (r *Rover) GoEast(step int8) {
+	r.LocY += step
 }
 
 // GoWest takes the rover west
-func (r *Rover) GoWest() {
-	r.LocY--
+func (r *Rover) GoWest(step int8) {
+	r.LocY -= step
 }
 
 // GoNorth takes the rover north
-func (r *Rover) GoNorth() {
-	r.LocX++
+func (r *Rover) GoNorth(step int8) {
+	r.LocX += step
 }
 
 // GoSouth takes the rover south
-func (r *Rover) GoSouth() {
-	r.LocX--
+func (r *Rover) GoSouth(step int8) {
+	r.LocX -= step
 }
 
 // Report outputs the current status of rover
@@ -64,13 +69,13 @@ func (r *Rover) TouchDown() string {
 }
 
 // Forward moves the rover one step in extant direction
-func (r *Rover) Forward() string {
-	r.Drive()
+func (r *Rover) Forward() (out string) {
+	r.Drive("F")
 	return r.Report()
 }
 
 // Backward moves the rover one step against extant direction
-func (r *Rover) Backward() string {
-	r.Drive()
+func (r *Rover) Backward() (out string) {
+	r.Drive("B")
 	return r.Report()
 }
